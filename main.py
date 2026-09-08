496754c7a46a2bdef2dc769e8a435625e5a58370
averageatbest <octaviogamez466@gmail.com>
2026-09-08 00:03:24 -0500

    feat: increase OCR dpi for better word recognision

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ for page_num, page in enumerate(doc):
 
     text = page.get_text()
 
-    # No embedded text - OCR the page
     if not text.strip():
         print(f"OCRing page {page_num + 1}...")
 
-        text_page = page.get_textpage_ocr()
+        text_page = page.get_textpage_ocr(
+            language="eng",
+            dpi=300,
+            full=True
+        )
+
         text = page.get_text(textpage=text_page)
 
     print(f"--- Page {page_num + 1} ---")
